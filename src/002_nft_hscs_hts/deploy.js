a307a90c21e4148ff22bf964ca7970cf74dbc026
Francesco Coacci <francescocoacci@admins-MBP-3.lan>
2022-05-27 19:52:33 +0200

    Refactoring

--- src/002_nft_hscs_hts/deploy.js
+++ src/002_nft_hscs_hts/deploy.js
@@ -4,27 +4,44 @@ const { AccountId,
         Client, 
         ContractCreateFlow, 
         ContractExecuteTransaction,
-        ContractFunctionParameters
+        ContractFunctionParameters,
+        AccountCreateTransaction,
+        Hbar
         } = require('@hashgraph/sdk');
 
 // Setup your .env path
 require('dotenv').config({path: __dirname + '/../../.env'});
 
+// CID from ipfs
+metadata = "QmdJwEyyZo8Vc7C7oq6vv1j3gw6a9Xaa9TR5cDudX4kKBW";
+
+const operatorKey = PrivateKey.fromString(process.env.PRIVATE_KEY);
+const operatorId = AccountId.fromString(process.env.ACCOUNT_ID);
+
+const client = Client.forTestnet().setOperator(operatorId, operatorKey);
+
+// Account creation function
+async function accountCreator(pvKey, iBal) {
+    const response = await new AccountCreateTransaction()
+        .setInitialBalance(new Hbar(iBal))
+        .setKey(pvKey.publicKey)
+        .execute(client);
+    const receipt = await response.getReceipt(client);
+    return receipt.accountId;
+}
+
 const main = async () => {
-
-    const accountId = AccountId.fromString(process.env.ACCOUNT_ID);
-    const privateKey = PrivateKey.fromString(process.env.PRIVATE_KEY);
-
-    metadata = "QmdJwEyyZo8Vc7C7oq6vv1j3gw6a9Xaa9TR5cDudX4kKBW";
-
-    const client = Client.forTestnet().setOperator(accountId, privateKey);
+    
+    // Init Alice account
+    const aliceKey = PrivateKey.generate();
+    const aliceId = await accountCreator(aliceKey, 100);
 
     const bytecode = fs.readFileSync('./binaries/NftManager_sol_NftManager.bin');
 
     // Create contract
     const createContract = new ContractCreateFlow()
-        .setGas(150000)
-        .setBytecode(bytecode);
+        .setGas(150000) // Increase if revert
+        .setBytecode(bytecode); // Contract bytecode
     const createContractTx = await createContract.execute(client);
     const createContractRx = await createContractTx.getReceipt(client);
     const contractId = createContractRx.contractId;
@@ -38,11 +55,11 @@ const main = async () => {
         .setPayableAmount(20) // Increase if revert
         .setFunction("createNft", 
             new ContractFunctionParameters()
-            .addString("CryptoFrancis")
-            .addString("CRYF")
-            .addString("Just a memo")
-            .addUint32(10)
-            .addUint32(7000000) // Needs to be between 6999999 and 8000001
+            .addString("CryptoFrancis") // NFT name
+            .addString("CRYF") // NFT symbol
+            .addString("Just a memo") // NFT memo
+            .addUint32(10) // NFT max supply
+            .addUint32(7000000) // Expiration: Needs to be between 6999999 and 8000001
             );
     const createTokenTx = await createToken.execute(client);
     const createTokenRx = await createTokenTx.getRecord(client);
@@ -57,7 +74,7 @@ const main = async () => {
         .setGas(1000000)
         .setFunction("mintNft",
             new ContractFunctionParameters()
-            .addAddress(tokenIdSolidityAddr) // Your token address
+            .addAddress(tokenIdSolidityAddr) // Token address
             .addBytesArray([Buffer.from(metadata)]) // Metadata
             );
     const mintTokenTx = await mintToken.execute(client);
@@ -65,16 +82,18 @@ const main = async () => {
     const serial = mintTokenRx.contractFunctionResult.getInt64(0);
 
     console.log(`Minted NFT with serial: ${serial} \n`);
-
-    // Transfer NFT
-    const transferToken = new ContractExecuteTransaction()
+    
+    // Transfer NFT to Alice
+    const transferToken = await new ContractExecuteTransaction()
         .setContractId(contractId)
         .setGas(1000000)
         .setFunction("transferNft",
             new ContractFunctionParameters()
-            .addAddress(tokenIdSolidityAddr)
-            .addAddress(accountId.toSolidityAddress())
-            .addInt64(serial))
+            .addAddress(tokenIdSolidityAddr) // Token address
+            .addAddress(aliceId.toSolidityAddress()) // Token receiver (Alice)
+            .addInt64(serial)) // NFT serial number
+        .freezeWith(client) // freezing using client
+        .sign(aliceKey); // Sign transaction with Alice 
     const transferTokenTx = await transferToken.execute(client);
     const transferTokenRx = await transferTokenTx.getReceipt(client);
 
